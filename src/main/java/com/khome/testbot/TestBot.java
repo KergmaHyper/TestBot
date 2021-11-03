c935604f88087342a1fb44f0c52fb9ab6e6174be
kergma@gmail.com <kergma@gmail.com>
2021-11-03 17:59:26 +0200

    work answer papuga bot

---
+++ src/main/java/com/khome/testbot/TestBot.java
@@ -0,0 +1,82 @@
+package com.khome.testbot;
+
+
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import lombok.SneakyThrows;
+
+
+public class TestBot extends TelegramLongPollingBot
+{
+  
+    @Override
+    @SneakyThrows
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()){
+                System.out.println(
+                    message.getChat().getId() + " " +
+                    message.getMessageId()+" "
+                    );
+
+                execute(
+                    SendMessage.builder()
+                    .chatId(message.getChatId().toString())
+                    .text(
+                        "you send: \n\n"+
+                        "\"" + 
+                        message.getText() + "\n"+ 
+                        "\""+
+                        "Your name: "+message.getChat().getFirstName()
+                        )
+                    .replyToMessageId(message.getMessageId())
+                    .build());
+            }
+        }        
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "KergmaHyper_bot";
+    }
+    interface Shifer {
+        char calc(int n1);
+       }
+    @Override
+    public String getBotToken() {
+
+       String tokStr =":8;1<:0;:<2IIMaAr{@KBPk:pXJC~~B;_aq}F%JRbc^:iA";
+       int keyI = 8;
+       Shifer xor = (a)-> (char) (a ^ keyI);
+       String tempStr = "";
+
+       for(int intCh: tokStr.getBytes() ){
+        tempStr += xor.calc(intCh);
+       }
+
+       return tempStr;
+
+      //  return "2039428324:AAEiIzsHCJXc2xPBKvvJ3WiyuN-BZjkV2aI";
+
+
+    }
+
+   
+
+    @SneakyThrows
+    public static void main( String[] args ) 
+    {
+       TestBot bot = new TestBot();
+       TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+       telegramBotsApi.registerBot(bot);
+
+    }
+
+}
---
+++ src/test/java/com/khome/testbot/TestBotTest.java
@@ -0,0 +1,20 @@
+package com.khome.testbot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestBotTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
