f296bd6e549926b86d09c8265e94b9460ccb8ad5
kergma@gmail.com <kergma@gmail.com>
2021-11-04 18:04:32 +0200

    with button ver 2

--- src/main/java/com/khome/service/CurrencyModeService.java
+++ src/main/java/com/khome/service/CurrencyModeService.java
@@ -2,8 +2,6 @@ package com.khome.service;
 
 import com.khome.entity.Currency;
 
-// import com.khome.entity.Currency;
-
 public interface CurrencyModeService {
 
     // static CurrencyModeService getInstance(){return new HashMapCurrencyModeService();}
@@ -11,4 +9,6 @@ public interface CurrencyModeService {
     Currency getTargetCurrency(long chatId);
     void setOriginalCurrency(long chatId, Currency currency);
     void setTargetCurrency(long chatId, Currency currency);
+    static CurrencyModeService getInstance(){return null;}
+    
 }
