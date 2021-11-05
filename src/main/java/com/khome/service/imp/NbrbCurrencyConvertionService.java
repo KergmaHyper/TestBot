eded43975396f3511b7a05a3bf5a19986eb988a6
kergma@gmail.com <kergma@gmail.com>
2021-11-05 15:11:47 +0200

    + .gitignore

---
+++ src/main/java/com/khome/service/imp/NbrbCurrencyConvertionService.java
@@ -0,0 +1,14 @@
+package com.khome.service.imp;
+
+import com.khome.entity.Currency;
+import com.khome.service.CurencyConvertionService;
+
+public class NbrbCurrencyConvertionService  implements CurencyConvertionService{
+
+    @Override
+    public double getCurrencyConversion(Currency originCurrency, Currency targetCurrency) {
+        // TODO Auto-generated method stub
+        return 1;
+    }
+    
+}
